f7c0a406c4f34812ae55738f59b1c8f3894f73b6
Eugenio <culurciello@gmail.com>
2013-11-09 17:22:28 -0500

    no inline c version

--- face-detector/run.lua
+++ face-detector/run.lua
@@ -6,9 +6,6 @@
 -- Clement Farabet
 --
 
-require 'xlua'
-require 'torch'
-require 'inline'
 require 'camera'
 require 'nnx'
 require 'luasdl'
@@ -32,45 +29,22 @@ torch.setnumthreads(4)
 
 luasdl.init(640, 480, 'face detector')
 
--- blob parser
-parse = inline.load [[
-      // get args
-      const void* id = luaT_checktypename2id(L, "torch.FloatTensor");
-      THFloatTensor *tensor = luaT_checkudata(L, 1, id);
-      float threshold = lua_tonumber(L, 2);
-      int table_blobs = 3;
-      int idx = lua_objlen(L, 3) + 1;
-      float scale = lua_tonumber(L, 4);
-
-      // loop over pixels
-      int x,y;
-      for (y=0; y<tensor->size[0]; y++) {
-         for (x=0; x<tensor->size[1]; x++) {
-            float val = THFloatTensor_get2d(tensor, y, x);
-            if (val > threshold) {
-               // entry = {}
-               lua_newtable(L);
-               int entry = lua_gettop(L);
-
-               // entry[1] = x
-               lua_pushnumber(L, x);
-               lua_rawseti(L, entry, 1);
-
-               // entry[2] = y
-               lua_pushnumber(L, y);
-               lua_rawseti(L, entry, 2);
-
-               // entry[3] = scale
-               lua_pushnumber(L, scale);
-               lua_rawseti(L, entry, 3);
-
-               // blobs[idx] = entry; idx = idx + 1
-               lua_rawseti(L, table_blobs, idx++);
-            }
-         }
-      }
-      return 0;
-]]
+-- blob parser:
+function parse(tin, threshold, blobs, scale)
+  --loop over pixels
+  for y=1, tin:size(1) do
+     for x=1, tin:size(2) do
+        local val = tin[y][x]
+        if (val > threshold) then               
+          entry = {}
+          entry[1] = x
+          entry[2] = y
+          entry[3] = scale
+          table.insert(blobs,entry)
+      end
+    end
+  end
+end
 
 -- load pre-trained network from disk
 network = nn.Sequential()
